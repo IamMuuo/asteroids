cf016f5875015f0fbff6f308a5c7322ada00a975
Erick <hearteric57@gmail.com>
2023-05-29 19:07:58 +0300

    feat: added player pause functionality

--- scripts/stage.gd
+++ scripts/stage.gd
@@ -5,7 +5,6 @@ const SCREEN_WIDTH = 320
 const SCREEN_HEIGHT = 180
 var score = 0
 
-
 var restart = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,11 +12,13 @@ func _ready():
 	get_node("spawn_timer").connect("timeout", _on_spawn_timer_timeout,)
 	get_node("player").connect("destroyed", _on_player_destroyed)
 	get_node("player").connect("hit", _on_player_hit)
-
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_key_pressed(KEY_ESCAPE):
+		get_node("pause_menu").visible = true
+		get_tree().paused = true
 
 func _on_spawn_timer_timeout():
 	var asteroid_instance = asteroid.instantiate()
@@ -36,3 +37,12 @@ func _on_player_hit():
 # Handle player being destroyed
 func  _on_player_destroyed():
 	get_node("ui/retry").visible = true
+
+
+func _on_quit_pressed():
+	get_tree().exit()
+
+
+func _on_resume_pressed():
+	get_node("pause_menu").visible = false
+	get_tree().paused = false
