69b80b9e14e1609805eee47f585f809df18f3c86
Erick <hearteric57@gmail.com>
2023-07-03 20:15:52 +0300

    feat: added user input

--- scripts/mainmenu.gd
+++ scripts/mainmenu.gd
@@ -1,9 +1,13 @@
 extends CanvasLayer
 
 
+const  SAVE_FILE_PATH = "user://asteroid.save"
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if !FileAccess.file_exists(SAVE_FILE_PATH):
+		# Hide the start button
+		get_node("buttons").visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,3 +21,27 @@ func _on_start_pressed():
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+func _on_name_input_text_changed():
+	var nameInput = get_node("setup/nameInput")
+	
+	if nameInput.text.length() > 1:
+		get_node("setup/join").visible = true
+	else:
+		get_node("setup/join").visible = false
+
+
+func _on_join_pressed():
+	var db = {"username": get_node("setup/nameInput").text}
+	# create the user save file
+	var user_stat_file = FileAccess.open(SAVE_FILE_PATH, FileAccess.WRITE_READ)
+	
+	if user_stat_file.is_open():
+		user_stat_file.store_var(db,true)
+	else:
+		print_debug(FileAccess.get_open_error())
+		
+	# display the intro text
+	
+	pass
